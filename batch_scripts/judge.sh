08fa4e9144d47a69d5129376b8f3f449c28a2298
Dongzhi Jiang <61070693+CaraJ7@users.noreply.github.com>
2025-08-05 10:54:26 +0800

    Update judge.sh

--- batch_scripts/judge.sh
+++ batch_scripts/judge.sh
@@ -1,7 +1,12 @@
+data_path=$1
+cache_dir=$2
+
+echo $data_path 
+echo $cache_dir
 
 export OPENAI_API_KEY=YOUR_API_KEY
 python main.py --name judge --num_threads 10 \
 --prompt_path prompt/prompt_judge.txt \
---data_path data/debug_data.json \
---cache_dir cache_debug/judge \
+--data_path results/xlsx/$data_path \
+--cache_dir $cache_dir \
 --model gpt-4o-mini
